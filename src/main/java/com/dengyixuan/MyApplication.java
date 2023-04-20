24882019776e9aecd83c1d9c3f568e5df53436f4
Yixuan <yxd105@student.hham.ac.uk>
2023-04-20 11:54:12 +0800

    MyMybatis

---
+++ src/main/java/com/dengyixuan/MyApplication.java
@@ -0,0 +1,13 @@
+package com.dengyixuan;/**
+ * @Author 85067
+ * @create 20/04/2023 11:50
+ */
+
+/**
+ * @author 85067
+ * @version 1.0
+ * @description: TODO
+ * @date 20/04/2023 11:50
+ */
+public class MyApplication {
+}
---
+++ src/main/java/com/dengyixuan/UserMapper.java
@@ -0,0 +1,8 @@
+package com.dengyixuan;
+
+/**
+ * @Author 85067
+ * @create 20/04/2023 11:51
+ */
+public interface UserMapper {
+}
